8e4100ce7c2a1e0448c42b0ed0d98a2d0b46f271
sbx_user1051 <sbx_user1051@169.254.181.250>
2025-01-16 15:06:41 +0000

    Thu Jan 16 15:06:41 UTC 2025

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,9 +1,11 @@
 from lib.solutions.CHK import checkout_solution
 
 
-class TestSum():
+class TestChk():
     def test_chkEmpty(self):
         assert checkout_solution.checkout("") == 0
+    def test_chkInvalid(self):
+        assert checkout_solution.checkout("Z") == -1
     def test_chk3As(self):
         assert checkout_solution.checkout("AAA") == 130
     def test_chk2Bs(self):
@@ -12,20 +14,18 @@ class TestSum():
         assert checkout_solution.checkout("ABCD") == 115
     def test_dealsGalore(self):
         assert checkout_solution.checkout("AAAAAABBBB") == 350
-class TestSum():
-    def test_chkEmpty(self):
-        assert checkout_solution.checkout("") == 0
-    def test_chk3As(self):
-        assert checkout_solution.checkout("AAA") == 130
-    def test_chk2Bs(self):
-        assert checkout_solution.checkout("BB") == 45
-    def test_chkNormal(self):
-        assert checkout_solution.checkout("ABCD") == 115
-    def test_dealsGalore(self):
-        assert checkout_solution.checkout("AAAAAABBBB") == 350
+    def test_2Es1BFree(self):
+        assert checkout_solution.checkout("EEEEBB") == 160
+    def test_2Es1BFree(self):
+        assert checkout_solution.checkout("EEEEBB") == 160
+    def test_9As(self):
+        assert checkout_solution.checkout("AAAAAAAAA") == 160
 
-TestSum().test_chkEmpty()
-TestSum().test_chk3As()
-TestSum().test_chk2Bs()
-TestSum().test_chkNormal()
-TestSum().test_dealsGalore()
+TestChk().test_chkEmpty()
+TestChk().test_chkInvalid()
+TestChk().test_chk3As()
+TestChk().test_chk2Bs()
+TestChk().test_chkNormal()
+TestChk().test_dealsGalore()
+TestChk().test_dealsGalore()
+TestChk().test_dealsGalore()
