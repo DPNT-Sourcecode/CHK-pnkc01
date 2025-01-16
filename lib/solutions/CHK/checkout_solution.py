5f8654172dc8233b853402f726b6ac7af555dd59
sbx_user1051 <sbx_user1051@169.254.181.250>
2025-01-16 14:54:41 +0000

    Thu Jan 16 14:54:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,32 +3,35 @@ from collections import defaultdict
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-skuMap = {
+sku_map = {
     'A': 50,
     'B': 30,
     'C': 20,
-    'D': 15
+    'D': 15,
+    'E': 40
 }
 
 
 def checkout(skus):
-    skuCounts = defaultdict(int)
+    sku_counts = defaultdict(int)
     totalCost = 0
     for c in skus:
-        if c not in skuMap:
+        if c not in sku_map:
             return -1
-        skuCounts[c] += 1
+        sku_counts[c] += 1
 
-    for k, v in skuCounts.items():
-        
+    number_of_free_Bs = sku_counts['E'] // 2
+    sku_counts['B'] -= number_of_free_Bs
 
-    for k, v in skuCounts.items():
+    for k, v in sku_counts.items():
         if k == 'A':
-            totalCost += (v % 3) * skuMap[k] + (v //3) * 130
+            totalCost += (v % 3) * sku_map[k] + (v //3) * 130
         elif k == 'B':
-            totalCost += (v % 2) * skuMap[k] + (v // 2) * 45
+            totalCost += (v % 2) * sku_map[k] + (v // 2) * 45
         else:
-            totalCost += v * skuMap[k]
+            totalCost += v * sku_map[k]
     
     return totalCost
+
+def 
 
