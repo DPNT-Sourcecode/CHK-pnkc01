f9b538e7c6433b3198bc527c274c3f6b7a1c7908
sbx_user1051 <sbx_user1051@169.254.181.250>
2025-01-16 17:20:33 +0000

    Thu Jan 16 17:20:33 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -86,8 +86,14 @@ def use_buy_get_free_deal(sku_counts, sku, buy_get_free_deal):
     count = sku_counts[sku]
     free_count = 0
 
+    if other_sku == sku:
+        a = sku_counts[sku] // (buy + free)
+        free_count = a * free
+        sku_
+
     if other_sku in sku_counts:
         free_count = (sku_counts[other_sku] // buy) * free
 
     sku_counts[sku] = max(0, count - free_count)
+
 
